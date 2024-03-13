27be5290ec5b30ae5f5d6f8ef17a39341cf7d387
erdemirtas <44425875+erdemirtas@users.noreply.github.com>
2024-03-13 12:51:56 +0300

    Revert "kerem"

--- strategybuilder/src/main/java/com/gridy/strategybuilder/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.gridy.strategybuilder.service;
-
-import com.gridy.strategybuilder.dto.UserDTO;
-import com.gridy.strategybuilder.dto.core.ResponsePayload;
-
-public interface UserService {
-
-  ResponsePayload<UserDTO> save(UserDTO userDTO);
-}
