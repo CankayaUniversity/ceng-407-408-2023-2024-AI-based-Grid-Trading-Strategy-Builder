740a5dd3e444f476e2f54fdccc85b3375be55d0b
erdem <bvolkan.erdogan@vbt.com.tr>
2024-05-28 21:04:50 +0300

    ++

--- src/app/grafik/grafik.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { GrafikComponent } from './grafik.component';
-@NgModule({
-  declarations: [],
-  imports: [CommonModule],
-})
-export class GrafikModule {}
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module'; // AppModule import'ini kontrol edin
-import { enableProdMode } from '@angular/core';
-
-// enableProdMode(); // Prodüksiyon modunda kullanmak istiyorsanız bu satırı açın.
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
