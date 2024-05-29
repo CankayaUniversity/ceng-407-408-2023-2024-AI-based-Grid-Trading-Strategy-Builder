9c5af9db8590ed04c87c8ad1be46201e54329588
aydolu <aydolukonca@gmail.com>
2024-05-29 10:43:19 +0300

    loginpage
    added new version

--- frontend/login/src/app/app.component.ts
+++ frontend/login/src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TokenStorageService } from './_services/token-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gridy';
+  private roles: string[] = [];
+  isLoggedIn = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService,    private router: Router, 
+    ) { 
+    console.log("constructordayız")
+  }
+
+
+  ngOnInit(): void {
+    console.log("ngonint")
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.username = user.username;
+    }  
+    else{
+      this.router.navigate(['/login']);
+
+    }
+
+  }
+
+  logout(): void {
+    console.log("logout")
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
